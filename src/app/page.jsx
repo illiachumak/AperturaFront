c2ae9c7fab0703f0f3dae0cdd2a0a1f7a414fabb
illiachumak <illia.chumak.2017@gmail.com>
2023-11-17 21:54:26 +0200

    comm

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,11 @@
 import Link from "next/link"
-import BG from '../../public/wooden.png'
 export default function Home() {
 
 /*
   * Resposible for routing and rendering pages 
 */
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-wooden" style={{backgroundImage: `src:(${BG})`,height: "972px"}}>
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Link href='/shop'>hello</Link>
     </main>
   )
