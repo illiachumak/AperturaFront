e1832307e3aaf409bbdcd10fc1d393f54b42d4bb
Vitalii Lutsyk <vitaliilutsyk9895@gmail.com>
2023-11-19 21:10:50 +0200

    main_page

--- src/app/Components/Carrousel/index.jsx
+++ src/app/Components/Carrousel/index.jsx
@@ -1,13 +1,30 @@
+import Image from 'next/image'
+import Link from 'next/link'
 
+import carousel from '../../assets/shop/carousel.jpg'
+
+import './style.css'
 /*
     * Responsible for rendering Carrousel Section
 */
 
-export default function Navbar() {
+
+export default function Carousel() {
 
   return (
-    <main className="p-4">
-      hello its Navbar
-    </main>
+    <>
+      <div className="carousel border-2 border-solid border-yellow-700 shop-wrapper">
+        <div id="item1" className="carousel-item w-full flex align-middle justify-center"> 
+          <Image src={carousel} className=" w-full object-cover h-484" alt='carousel-image'/>
+          <div className='flex absolute flex-col self-center z-0 text-center'>
+            <h2 className='main_text'>Ласкаво просимо до Apertura - де дизайн зустрічається з майстерністю.</h2>
+            <Link href='/shop'><button className='main_button border-yellow-600'>Магазин</button></Link>
+          </div>
+        </div> 
+        <div id="item2" className="carousel-item w-full">
+          <Image src={carousel} className="w-full object-cover h-484" height={480} alt='carousel-image'/>
+        </div> 
+      </div> 
+    </>
   )
 }
--- src/app/Components/Navbar/index.jsx
+++ src/app/Components/Navbar/index.jsx
@@ -9,51 +9,31 @@ import './style.css'
 
 export default function Navbar() {
 
-      return (<>
-        <nav className="flex min-w-fit w-full  bg-[#130E04] py-5 justify-around text-white font-sans">
-          <div className="px-5">
+      return (<div className = 'nav bg-[#130E04] flex flex-col align-middle'>
+        <nav className="flex py-5 text-white font-sans justify-between shop-wrapper">
+          <div className="px-2">
             <Link href='/'><Image src={Logo} width={60} height={60}/></Link>
           </div>
-          <div className="flex-none">
-            <ul className="menu menu-horizontal px-5 divide-x divide-slate-600">
-              <li><Link href='/' className=" hover:bg-tranhover:text-yellow-500sparent mx-3">Магазин</Link></li>
+          <div className="flex px-0">
+            <ul className="menu menu-horizontal divide-x divide-slate-600 self-center px-0">
+              <li><Link href='/shop' className="hover:text-yellow-500 hover:bg-tranhover:text-yellow-500sparent mx-3">Магазин</Link></li>
               <li><Link href='/' className="hover:text-yellow-500 hover:bg-transparent mx-3">Зворотній зв'язок</Link></li>
               <li><Link href='/' className="hover:text-yellow-500 hover:bg-transparent mx-3">0665763845</Link></li>
               <li><Link href='/' className="hover:text-yellow-500 hover:bg-transparent mx-3">Корзина</Link></li>
             </ul>
           </div>
         </nav>
-        <div className="dropdown dropdown-hover w-full">
-          <ul className="py-2 menu menu-horizontal flex justify-around min-w-fit px-52 bg-[#3A2911] text-white font">
-            <Link href='/'><li tabIndex={0} className="onH hover:text-yellow-500">Двері</li></Link>
-            <Link href='/'><li className="hover:text-yellow-500"> Стінові панелі</li></Link>
-            <Link href='/'><li className="hover:text-yellow-500">Фурнітура</li></Link>
-            <Link href='/'><li className="hover:text-yellow-500"> Профіль</li></Link>
-          </ul>
-          <ul tabIndex={0} className="dropdown-content z-[1] menu-horizontal p-2 shadow bg-[#3A2911] w-full min-w-fit px-48 flex justify-around text-white leading-7">
-            <ul>
-              <li><Link href='/' className="">Вхідні двері</Link></li>
-              <li><Link href='/'>Приховані двері</Link></li>
-              <li><Link href='/'>Розсувні двері</Link></li>
-              <li><Link href='/'>Маятникові двері</Link></li>
-              <li><Link href='/'>Рото-двері</Link></li>
-              <li><Link href='/'>Compack двері</Link></li>
-            </ul>
-            <ul>
-            </ul>
-            <ul>
-            </ul>
-            <ul>
-              <li><Link href='/'>Ручки</Link></li>
-              <li><Link href='/'>Замки</Link></li>
-              <li><Link href='/'>Петлі</Link></li>
-              <li><Link href='/'>Стопор</Link></li>
-              <li><Link href='/'>Поріг</Link></li>
-              <li><Link href='/'>Системи</Link></li>
-            </ul>
-          </ul>
-        </div>
-        </>
+        <div className="bg-[#3A2911] flex flex-col">
+            <div className="dropdown dropdown-hover">
+              <ul className="dropdown dropdown-hover py-5 menu menu-horizontal flex justify-between min-w-fit px-12 text-white shop-wrapper font">
+                <Link href='/'><li className="peer hover:text-yellow-500">Двері</li></Link>
+                <Link href='/'><li className="hover:text-yellow-500">Стінові панелі</li></Link>
+                <Link href='/'><li className="hover:text-yellow-500">Профіль</li></Link>
+                <Link href='/'><li className="hover:text-yellow-500">Фурнітура</li></Link>
+              </ul>
+            </div>
+            </div>
+          </div>
       )
     }
     
