a2764d666ed605eb8f5bab7444bef5b718819911
illiachumak <illia.chumak.2017@gmail.com>
2024-01-16 23:57:18 +0200

    paths fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
     images: {
         domains: ['apertura-s3-photos.s3.amazonaws.com'],
         formats: ["image/webp"],
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "apertura-s3-photos.s3.amazonaws.com",
+            port: "",
+            pathname: "/**",
+          },
+        ],
       },
 }
 
